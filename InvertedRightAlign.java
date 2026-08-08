c66c1a0c6754c500b288637577a005121a7adfdc
mgowri1305-maker <mgowri1305@gmail.com>
2026-08-08 18:54:10 +0530

    Update InvertedRightAlign.java

--- InvertedRightAlign.java
+++ InvertedRightAlign.java
@@ -1,1 +1,21 @@
+class pattern7
+{   
+ public static void main(String[] args) {
+int i,j;
+int n=5;
+for(i=5;i>=1;i--)
+{
+    int colspace=n-i;
+    for(int s=1;s<=colspace;s++)
+    System.out.print(" ");
 
+    for(j=1;j<=i;j++)
+    {
+     System.out.print("*");
+    }
+    System.out.println();
+}
+ System.out.println();
+
+ }
+}
