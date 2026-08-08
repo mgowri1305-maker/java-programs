734d7d8bf313f40134b9248d850899906a6ee79f
mgowri1305-maker <mgowri1305@gmail.com>
2026-08-08 18:26:19 +0530

    Create InvertedRightTri.java

---
+++ InvertedRightTri.java
@@ -0,0 +1,16 @@
+class pattern
+{   
+ public static void main(String[] args) {
+int i,j;
+int n=5;for(i=n;i>=1;i--)
+{
+    for(j=1;j<=i;j++)
+    {
+        System.out.print("* ");
+    }
+     System.out.println(" ");
+}
+ System.out.println("");
+
+  }
+  }
