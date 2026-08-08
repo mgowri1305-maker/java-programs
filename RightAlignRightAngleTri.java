9c19d7571f3ced4dd9839f2bd3b6a36896c797c6
mgowri1305-maker <mgowri1305@gmail.com>
2026-08-08 18:37:20 +0530

    Create RightAlignRightAngleTri.java

---
+++ RightAlignRightAngleTri.java
@@ -0,0 +1,21 @@
+class pattern6
+{   
+ public static void main(String[] args) {
+int i,j;
+int n=5;
+for(i=1;i<=n;i++)
+{
+    int colspace=n-i;
+    for(int s=1;s<=colspace;s++)
+    System.out.print(" ");
+
+    for(j=1;j<=i;j++)
+    {
+     System.out.print("*");
+    }
+    System.out.println();
+}
+ System.out.println();
+
+ }
+}
