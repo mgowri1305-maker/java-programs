6791094c033dcd343a5aeaeb57ac8e3c93c26dbd
mgowri1305-maker <mgowri1305@gmail.com>
2026-08-08 18:29:17 +0530

    Create NumberTri.java

---
+++ NumberTri.java
@@ -0,0 +1,17 @@
+class pattern
+{   
+ public static void main(String[] args) {
+int i,j;
+int n=5;
+for(i=1;i<=n;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        System.out.print(j +" ");
+    }
+     System.out.println("");
+}
+ System.out.println("");
+
+  }
+  }
