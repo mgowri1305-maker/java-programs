1b0fff30a53f75c981b1ddb7a4ed29181eca39a0
mgowri1305-maker <mgowri1305@gmail.com>
2026-08-08 18:23:18 +0530

    Create RightAngleTri.java

---
+++ RightAngleTri.java
@@ -0,0 +1,18 @@
+class pattern
+{   
+ public static void main(String[] args) 
+  {
+int i,j;
+int n=5;
+for(i=1;i<=n;i++)
+{
+    int col=i;
+    for(j=1;j<=col;j++)
+    {
+        System.out.print("* ");
+    }
+     System.out.println(" ");
+}
+  System.out.println("");
+  }
+  }
